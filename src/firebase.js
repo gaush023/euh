8e0299b121ecfba0d7b7063432c185093909f67b
揚村 周夏 <shuga@yousonnoMacBook-Pro.local>
2024-10-26 11:39:54 +0900

    update: firebase.js, add db

--- src/firebase.js
+++ src/firebase.js
@@ -22,5 +22,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
 export { auth, analytics };
+export default db;
